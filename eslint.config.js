60d10e1cdc2753636219ff6702987d6cfcb14e4a
Татьяна Андреева <tan-andreeva@mail.ru>
2025-04-14 14:57:47 +0400

    add prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,31 @@
-
-// import js from "@eslint/js";
 import globals from "globals";
-import tseslint from 'typescript-eslint';
+import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-    { files: ["**/*.{js,jsx,ts,tsx}"] },
-    { ignores: ['/node_modules/', 'dist/'] },
-    { 
-      languageOptions: {
-        ecmaVersion: 'latest',
-        globals: globals.browser,
-        sourceType: 'module',
+  { files: ["**/*.{js,jsx,ts,tsx}"] },
+  { ignores: ["/node_modules/", "dist/"] },
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: globals.browser,
+      sourceType: "module",
+    },
+  },
+  {
+    settings: {
+      react: {
+        version: "18.3",
       },
     },
-    {
-      settings: {
-        react: {
-          version: '18.3',
-        },
-      },
+  },
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    rules: {
+      semi: "error",
+      indent: ["error", 2],
+      "react/react-in-jsx-scope": "off",
     },
-    ...tseslint.configs.recommended,
-    pluginReact.configs.flat.recommended,
-    {
-      rules: {
-        semi: 'error',
-        'react/react-in-jsx-scope': 'off',
-        'react/prop-types': 'off',
-      },
-    },
+  },
 ];
