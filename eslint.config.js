28395f7ea9e5836c3b15fc49f0e4b9b589326bfa
Татьяна Андреева <tan-andreeva@mail.ru>
2025-04-14 14:32:05 +0400

    add eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+// import js from "@eslint/js";
+import globals from "globals";
+import tseslint from 'typescript-eslint';
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+    { files: ["**/*.{js,jsx,ts,tsx}"] },
+    { ignores: ['/node_modules/', 'dist/'] },
+    { 
+      languageOptions: {
+        ecmaVersion: 'latest',
+        globals: globals.browser,
+        sourceType: 'module',
+      },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+    {
+      settings: {
+        react: {
+          version: '18.3',
+        },
+      },
     },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+      rules: {
+        semi: 'error',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+      },
     },
-  },
-)
+];
